c7e5ef732e55c83b685568b5999a5d87d343775f
HUANG HSIANG <129297292+BennYYYYYYYYYYY@users.noreply.github.com>
2024-12-19 21:28:12 +0800

    Add files via upload

---
+++ MySQL/LeetCode/Curated SQL 70 Easy/1141. User Activity for the Past 30 Days I.sql
@@ -0,0 +1,33 @@
+USE leetcode;
+
+DROP TABLE IF EXISTS Activity;
+
+Create table If Not Exists Activity (user_id int, session_id int, activity_date date, activity_type ENUM('open_session', 'end_session', 'scroll_down', 'send_message'));
+Truncate table Activity;
+insert into Activity (user_id, session_id, activity_date, activity_type) values ('1', '1', '2019-07-20', 'open_session');
+insert into Activity (user_id, session_id, activity_date, activity_type) values ('1', '1', '2019-07-20', 'scroll_down');
+insert into Activity (user_id, session_id, activity_date, activity_type) values ('1', '1', '2019-07-20', 'end_session');
+insert into Activity (user_id, session_id, activity_date, activity_type) values ('2', '4', '2019-07-20', 'open_session');
+insert into Activity (user_id, session_id, activity_date, activity_type) values ('2', '4', '2019-07-21', 'send_message');
+insert into Activity (user_id, session_id, activity_date, activity_type) values ('2', '4', '2019-07-21', 'end_session');
+insert into Activity (user_id, session_id, activity_date, activity_type) values ('3', '2', '2019-07-21', 'open_session');
+insert into Activity (user_id, session_id, activity_date, activity_type) values ('3', '2', '2019-07-21', 'send_message');
+insert into Activity (user_id, session_id, activity_date, activity_type) values ('3', '2', '2019-07-21', 'end_session');
+insert into Activity (user_id, session_id, activity_date, activity_type) values ('4', '3', '2019-06-25', 'open_session');
+insert into Activity (user_id, session_id, activity_date, activity_type) values ('4', '3', '2019-06-25', 'end_session');
+
+
+WITH number_of_activities_by_date_user AS (
+	SELECT activity_date,
+		   user_id,
+		   count(*) AS number_of_activities # 例如 (2019-07-20, 1) 值有 3 組
+	FROM activity
+	WHERE activity_date BETWEEN date_sub('2019-07-27', INTERVAL 30 DAY) AND 
+										'2019-07-27'
+	GROUP BY activity_date, # activity_date 與 activity_type 組合為唯一值 
+		     user_id 
+)
+SELECT activity_date AS day,
+count(*) AS active_users
+FROM number_of_activities_by_date_user
+GROUP BY activity_date;
